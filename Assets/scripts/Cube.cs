e76e08a1a27fa9a8cc41ba53e8293a70f9df8726
LeonKir <110807806+LeonKir@users.noreply.github.com>
2024-11-22 13:10:23 +0000

    a

--- Assets/scripts/Cube.cs
+++ Assets/scripts/Cube.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 [RequireComponent(typeof(Renderer))]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(ColorChanger))]
+[RequireComponent(typeof(ExplosionHandler))]
+
 public class Cube : MonoBehaviour
 {
-    public GameObject cubePrefab;
+    [SerializeField] private Cube cubePrefab;
+
     private float _splitChance = 1.0f;
 
     private void OnMouseDown()
@@ -24,61 +28,48 @@ public class Cube : MonoBehaviour
     {
         int minCountCubes = 2;
         int maxCountCubes = 7;
+        float divisionSplitChance = 2.0f;
 
         int newCubesCount = Random.Range(minCountCubes, maxCountCubes);
         Vector3 originalPosition = transform.position;
         Vector3 originalScale = transform.localScale;
 
-        Debug.Log(_splitChance);
+        Debug.Log($"Split Chance: {_splitChance}");
 
         for (int i = 0; i < newCubesCount; i++)
         {
-            GameObject newCube = InitializationCubes(originalPosition, originalScale);
+            Cube newCube = InitializeCube(originalPosition, originalScale);
 
-            ChangeColor(newCube);
+            ColorChanger colorChanger = newCube.GetComponent<ColorChanger>();
 
-            Rigidbody rigidbody = newCube.GetComponent<Rigidbody>();
-            if (rigidbody != null)
+            if (colorChanger != null)
             {
-                ScatteringCubes(newCube, originalPosition, rigidbody);
+                colorChanger.ChangeColor();
             }
 
-            Cube newCubeScript = newCube.GetComponent<Cube>();
-            if (newCubeScript != null)
+            ExplosionHandler explosionHandler = newCube.GetComponent<ExplosionHandler>();
+
+            if (explosionHandler != null)
             {
-                newCubeScript._splitChance = _splitChance / 2.0f;
+                explosionHandler.ApplyExplosion(originalPosition);
             }
+
+            newCube._splitChance = _splitChance / divisionSplitChance;
         }
 
         Destroy(gameObject);
     }
 
-    private GameObject InitializationCubes(Vector3 originalPosition, Vector3 originalScale)
+    private Cube InitializeCube(Vector3 originalPosition, Vector3 originalScale)
     {
-        Vector3 newPosition = originalPosition + Random.insideUnitSphere * 0.5f;
-        GameObject newCube = Instantiate(cubePrefab, newPosition, Random.rotation);
+        float biasVector = 0.5f;
+        int biasScale = 2;
 
-        newCube.transform.localScale = originalScale / 2;
+        Vector3 newPosition = originalPosition + Random.insideUnitSphere * biasVector;
+        Cube newCube = Instantiate(cubePrefab, newPosition, Random.rotation);
+
+        newCube.transform.localScale = originalScale / biasScale;
 
         return newCube;
-    }
-
-    private void ChangeColor(GameObject newCube)
-    {
-        Renderer renderer = newCube.GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            Color randomColor = new Color(Random.value, Random.value, Random.value);
-            renderer.material.color = randomColor;
-        }
-    }
-
-    private void ScatteringCubes(GameObject newCube, Vector3 originalPosition, Rigidbody rigidbody)
-    {
-        float explosionForce = 300.0f;
-        float explosionRadius = 5.0f;
-
-        Vector3 explosionDirection = (newCube.transform.position - originalPosition).normalized;
-        rigidbody.AddExplosionForce(explosionForce, originalPosition, explosionRadius);
     }
 }
